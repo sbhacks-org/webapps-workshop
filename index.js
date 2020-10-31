86fc9d6f7879cfd4249fabaf989d93dcbbfda1fd
Jayleen Li <jayleenli17@gmail.com>
2020-10-31 14:27:45 -0700

    Add image src link changer

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// $("#create-meme").click(function () {
-//   const topText = $("#top-text-input").val();
-//   $("#top-text").text(topText);
-
-//   const bottomText = $("#bottom-text-input").val();
-//   $("#bottom-text").text(bottomText);
-// });
-
 document.getElementById("create-meme").addEventListener("click", function () {
   const topText = document.getElementById("top-text-input").value;
   document.getElementById("top-text").innerText = topText;
 
   const bottomText = document.getElementById("bottom-text-input").value;
   document.getElementById("bottom-text").innerText = bottomText;
+
+  const imgSrc = document.getElementById("img-src-input").value;
+  if (imgSrc) {
+  	document.getElementById("meme-pic").src = imgSrc;
+  }
 });
