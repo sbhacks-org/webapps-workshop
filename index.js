290a3d788bdec2d9d4c4c6dc7f4575d00598d85b
Jayleen Li <jayleenli17@gmail.com>
2020-11-05 00:17:04 -0800

    Add window.onload for JavaScript

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-document.getElementById("create-meme").addEventListener("click", function () {
-  const topText = document.getElementById("top-text-input").value;
-  document.getElementById("top-text").innerText = topText;
+window.onload = function() {
+	document.getElementById("create-meme").addEventListener("click", function () {
+	  const topText = document.getElementById("top-text-input").value;
+	  document.getElementById("top-text").innerText = topText;
 
-  const bottomText = document.getElementById("bottom-text-input").value;
-  document.getElementById("bottom-text").innerText = bottomText;
+	  const bottomText = document.getElementById("bottom-text-input").value;
+	  document.getElementById("bottom-text").innerText = bottomText;
 
-  const imgSrc = document.getElementById("img-src-input").value;
-  if (imgSrc) {
-  	document.getElementById("meme-pic").src = imgSrc;
-  }
-});
+	  const imgSrc = document.getElementById("img-src-input").value;
+	  if (imgSrc) {
+	  	document.getElementById("meme-pic").src = imgSrc;
+	  }
+	});
+}
